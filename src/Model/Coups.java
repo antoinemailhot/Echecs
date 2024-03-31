ac7d4cfee7f92935d6d57c7b77b6e90ea65688d8
Antoine Mailhot <capitaineam@outlook.com>
2024-03-31 09:16:34 -0400

    Ajout Coups.java dans model

---
+++ src/Model/Coups.java
@@ -0,0 +1,32 @@
+package Model;
+
+public abstract class Coups {
+    
+    // Constructeur.
+    public Coups() {}
+
+    /**
+     * Retourne si le coups est valide. 
+     * @return Retourne si le coups est valide. 
+     */
+    public abstract boolean estValide();
+
+    /**
+     * Retourne la case du déplacement.
+     * @return Retourne la case du déplacement.
+     */
+    public abstract Case deplacement();
+
+    /**
+     * Retourne la case de l'attaque.
+     * @return Retourne la case de l'attaque.
+     */
+    public abstract Case attaquer();
+
+    /**
+     * Retourne si la case est dangereuse pour la Piece.
+     * @return Retourne si la case est dangereuse pour la Piece.
+     */
+    public abstract boolean estCaseDangereuse();
+
+}
