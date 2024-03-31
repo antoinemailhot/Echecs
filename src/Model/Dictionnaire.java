efaaddacd1664c5bc515037a2e047e50d29594df
Antoine Mailhot <capitaineam@outlook.com>
2024-03-31 07:56:16 -0400

    Ajout de Dictionnaire.java dans model contenant enum

---
+++ src/Model/Dictionnaire.java
@@ -0,0 +1,23 @@
+package Model;
+
+public class Dictionnaire {
+    public static enum Couleur {
+        Noir,
+        Blanc
+    }
+
+    public static enum Type {
+        Pion,
+        Tour,
+        Chevalier,
+        Fou,
+        Reine,
+        Roi
+    }
+
+    public static enum Direction {
+        Direction_Origin_Blanc,
+        Direction_Origin_Noir
+    }
+    
+}
