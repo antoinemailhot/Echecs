c960876a4ed982f24bbf9c2548a053bf1941ce12
Antoine Mailhot <capitaineam@outlook.com>
2024-03-31 08:28:47 -0400

    AJout de Joueur.java dans model avec implementation

---
+++ src/Model/Joueur.java
@@ -0,0 +1,64 @@
+package Model;
+
+public class Joueur {
+    // Les attrubuts :
+    private String nom;
+    private Enum couleur;
+    private int victoires;
+
+    // Constructeur.
+    public Joueur(String nom, Enum couleur) {
+        this.nom = nom;
+        this.couleur = couleur;
+
+    }
+
+    /**
+     * Prends un nom.
+     * @param nom
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    /**
+     * Prends une couleur.
+     * @param couleur
+     */
+    public void setCouleur(Enum couleur) {
+        this.couleur = couleur;
+    }
+
+    /**
+     * Prends un nombre de victoire.
+     * @param victoires
+     */
+    public void setVictoires(int victoires) {
+        this.victoires = victoires;
+    }
+
+    /**
+     * Obtient le nom.
+     * @return Retourne le nom.
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * Obtient la couleur.
+     * @return Retourne la couleur.
+     */
+    public Enum getCouleur() {
+        return couleur;
+    }
+    
+    /**
+     * Obtient le nombre de victoire.
+     * @return Retourne le nombre de victoire.
+     */
+    public int getVictoires() {
+        return victoires;
+    }
+    
+}
