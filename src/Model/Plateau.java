7f1dc690e50f5a97cb86a7a71a266d733f42d884
Antoine Mailhot <capitaineam@outlook.com>
2024-03-31 06:19:20 -0400

    Ajout Plateau.java dans model

---
+++ src/Model/Plateau.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class Plateau {
+
+    // Les attributs :
+    private Case[] cases;
+    private Pieces[][] pieces;
+
+    // Le constructeur.
+    public Plateau() {
+        
+    }
+}
